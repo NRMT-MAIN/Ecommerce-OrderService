a9862dbdba4e0e73520f09b077e4b7d289d2555b
NRMT-MAIN <sahunirmit.main@gmail.com>
2025-07-25 20:10:29 +0530

    Added Eureke Client

--- src/main/java/com/example/ecommerceSpringOrderService/clients/ProductServiceClient.java
+++ src/main/java/com/example/ecommerceSpringOrderService/clients/ProductServiceClient.java
@@ -2,22 +2,20 @@ package com.example.ecommerceSpringOrderService.clients;
 
 
 import com.example.ecommerceSpringOrderService.dtos.ProductDTO;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ProductServiceClient {
-    private final RestTemplateBuilder restTemplateBuilder ;
+    private final RestTemplate restTemplate;
 
-    ProductServiceClient(RestTemplateBuilder restTemplateBuilder){
-        this.restTemplateBuilder = restTemplateBuilder ;
+    ProductServiceClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate ;
     }
 
     public ProductDTO getProductbyId(Long id){
-        RestTemplate restTemplate = restTemplateBuilder.build() ;
-        String url = "http://localhost:3000/api/v1/products/" + id ;
+        String url = "http://ecommerceSpring/api/v1/products/" + id ;
 
         ResponseEntity<ProductDTO> response = restTemplate.getForEntity(url , ProductDTO.class) ;
 
